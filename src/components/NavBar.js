96662933a7ed384f3388043093024ad811e16f9d
Nicolaev <nnicolae@ciena.com>
2023-07-18 14:53:39 -0400

    Modified toolbar, added shadow and properly aligned buttons.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,55 +1,65 @@
 import React from 'react'
 import CustomBtn from './CustomBtn'
-import {Toolbar, Typography} from '@material-ui/core'
+import {Toolbar, Typography, Box} from '@material-ui/core'
 import {makeStyles} from "@material-ui/core/styles"; 
 
 const styles = makeStyles({
     bar:{
-        paddingTop: "1.15rem",
+        boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.2)',
+        display: "flex",
+        justifyContent: "space-between",
+        paddingTop: "0.5rem",
         backgroundColor: "#fff",
         ['@media (max-width:780px)']: { 
            flexDirection: "column"
           }
+    },
+    box:{
+        display: "flex",
+        alignItems: "center"
     },
     logo: {
         width: "15%", 
         display: "inline-block"
     },
     menuItem: {
+        color:  "#696767",
         cursor: "pointer", 
-        flexGrow: 1,
+        flexGrow: 0.05,
         "&:hover": {
-            color:  "#4f25c8"
+            color:  "#800000"
         },
         ['@media (max-width:780px)']: { 
             paddingBottom: "1rem"    }
     }
 })
-//<img src={logoMobile} className={classes.logoMobile}/> 
+
 function NavBar() {
     const classes = styles()
     return (
             <Toolbar position="sticky" color="rgba(0, 0, 0, 0.87)" className={classes.bar}>   
-                <img src="j_and_s_logo.jpg" alt="logo" className={classes.logo}/> 
-                <Typography variant="h6" className={classes.menuItem}>
-                   Home
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    About Us
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Services
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Products 
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Careers 
-                </Typography>
-                <Typography variant="h6" className={classes.menuItem}>
-                    Contact Us 
-                </Typography>
-                <CustomBtn txt="Trial Our Product"/>
+                <Box className={classes.box}>
+                    <img src="j_and_s_logo.jpg" alt="logo" className={classes.logo}/> 
+                    <Box flexGrow={1}/>
+                    <Typography variant="h6" className={classes.menuItem}>
+                    Home
+                    </Typography>
+                    <Typography variant="h6" className={classes.menuItem}>
+                        About Us
+                    </Typography>
+                    <Typography variant="h6" className={classes.menuItem}>
+                        Services
+                    </Typography>
+                    <Typography variant="h6" className={classes.menuItem}>
+                        Products 
+                    </Typography>
+                    <Typography variant="h6" className={classes.menuItem}>
+                        Careers 
+                    </Typography>
+                    <Typography variant="h6" className={classes.menuItem}>
+                        Contact Us 
+                    </Typography>
+                </Box>
             </Toolbar>
     )
 }
