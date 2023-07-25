954d9007132390f38acec6b1b4c41997897c8d19
Nicholas Nicolaev <84345327+Ennics@users.noreply.github.com>
2023-07-24 22:29:08 -0400

    Updated the footer, still needs proper links and some styling modifications.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,190 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Typography, Box} from '@material-ui/core'; 
+import IconButton from '@material-ui/core/IconButton';
+import FacebookIcon from '@material-ui/icons/Facebook';
 import {makeStyles} from "@material-ui/core/styles"; 
 
-const styles = makeStyles({
-  footer: {
-    backgroundColor: '#f0f0f0',
-    padding: '20px',
-    textAlign: 'center',
-    marginTop: 'auto', // Push the footer to the bottom
-  },
-  links: {
-    margin: '0 10px',
-    textDecoration: 'none',
-    color: '#333',
-  },
-});
+// const useStyles = makeStyles((theme) => ({
+//   footer: {
+//     backgroundColor: '#3B0032', // Dark maroon background color
+//     color: '#fff', // Text color
+//     padding: theme.spacing(4), // Ample room around the content
+//     display: 'flex',
+//     justifyContent: 'center', // Center the sections horizontally
+//     alignItems: 'center', // Center the sections vertically
+//     flexDirection: 'row', // Arrange sections horizontally
+//   },
+//   section: {
+//     margin: theme.spacing(2), // Space between sections
+//     textAlign: 'center', // Center the content within each section
+//   },
+//   title: {
+//     fontFamily: 'Arial, sans-serif', // Replace with the desired font-family
+//     fontSize: '24px', // Replace with the desired font size
+//     fontWeight: 'bold', // Replace with the desired font weight
+//     marginBottom: theme.spacing(1), // Space between title and description
+//   },
+//   description: {
+//     fontSize: '16px', // Replace with the desired font size
+//   },
+//   link: {
+//     color: '#000', // Black color for the links
+//     textDecoration: 'none', // Remove underline
+//     fontFamily: 'Arial, sans-serif', // Replace with the desired font-family
+//     fontSize: '18px', // Replace with the desired font size
+//     margin: theme.spacing(1), // Space between each link
+//   },
+// }));
 
-function Footer() {
-  const classes = styles();
+// const Footer = () => {
+//   const classes = useStyles();
+
+//   return (
+//     <footer className={classes.footer}>
+//       {/* First Section */}
+//       <Box className={classes.section}>
+//         <Typography variant="h6" className={classes.title}>
+//           Our Company
+//         </Typography>
+//         <Typography variant="body1" className={classes.description}>
+//           A family-owned construction company with 25 years of experience.
+//         </Typography>
+//         <IconButton
+//           component="a"
+//           href="https://www.facebook.com/your-facebook-account/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <FacebookIcon style={{ color: '#fff' }} />
+//         </IconButton>
+//       </Box>
+
+//       {/* Second Section */}
+//       <Box className={classes.section}>
+//         <Typography variant="h6" className={classes.title}>
+//           Helpful Links
+//         </Typography>
+//         <Box display="flex" flexDirection="column" alignItems="center">
+//           <Typography variant="body1" className={classes.link}>
+//             Home
+//           </Typography>
+//           <Typography variant="body1" className={classes.link}>
+//             About Us
+//           </Typography>
+//           <Typography variant="body1" className={classes.link}>
+//             Services
+//           </Typography>
+//           <Typography variant="body1" className={classes.link}>
+//             Products
+//           </Typography>
+//           <Typography variant="body1" className={classes.link}>
+//             Careers
+//           </Typography>
+//           <Typography variant="body1" className={classes.link}>
+//             Contact Us
+//           </Typography>
+//         </Box>
+//       </Box>
+
+//       {/* Third Section */}
+//       <Box className={classes.section}>
+//         <Typography variant="h6" className={classes.title}>
+//           Contact Info
+//         </Typography>
+//         <Typography variant="body1">
+//           Email: contact@example.com
+//           <br />
+//           Phone: (123) 456-7890
+//         </Typography>
+//       </Box>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: '#666666', // Grey background color
+    color: '#fff', // Text color
+    padding: theme.spacing(4), // Ample room around the content
+    display: 'flex',
+    justifyContent: 'center', // Center the sections horizontally
+    alignItems: 'center', // Center the sections vertically
+    flexDirection: 'row', // Arrange sections horizontally
+  },
+  section: {
+    margin: theme.spacing(2), // Space between sections
+    textAlign: 'center', // Center the content within each section
+  },
+  title: {
+    fontFamily: 'Arial, sans-serif', // Replace with the desired font-family
+    fontSize: '24px', // Replace with the desired font size
+    fontWeight: 'bold', // Replace with the desired font weight
+    marginBottom: theme.spacing(1), // Space between title and description
+  },
+  description: {
+    fontSize: '16px', // Replace with the desired font size
+    maxWidth: '300px', // Limit the width of the description box
+    margin: '0 auto', // Center the description box horizontally
+  },
+  link: {
+    color: '#000', // Black color for the links
+    textDecoration: 'none', // Remove underline
+    fontFamily: 'Arial, sans-serif', // Replace with the desired font-family
+    fontSize: '18px', // Replace with the desired font size
+    margin: theme.spacing(1), // Space between each link
+  },
+}));
+
+const Footer = () => {
+  const classes = useStyles();
+
   return (
-    <div className={classes.footer}>
-      <p>© {new Date().getFullYear()} JASON'S LANDSCAPING INC.</p>
-      <div>
-        {/* Replace the Link components with simple text placeholders */}
-        <Link to="/" className={classes.links}>Home</Link>
-        <Link to="/about" className={classes.links}>About Us</Link>
-        <Link to="/services" className={classes.links}>Services</Link>
-        <Link to="/products" className={classes.links}>Products</Link>
-        <Link to="/careers" className={classes.links}>Careers</Link>
-        <Link to="/contact" className={classes.links}>Contact Us</Link>
-      </div>
-    </div>
+    <footer className={classes.footer}>
+      {/* First Section */}
+      <Box className={classes.section}>
+        <Typography variant="h6" className={classes.title}>
+          Our Company
+        </Typography>
+        <Typography variant="body1" className={classes.description}>
+          A family-owned construction company with 25 years of experience.
+        </Typography>
+        <IconButton
+          component="a"
+          href="https://www.facebook.com/jasonslandscapingandsnowplowing/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon style={{ color: '#fff' }} />
+        </IconButton>
+      </Box>
+
+      {/* Second Section */}
+      <Box className={classes.section}>
+        <Typography variant="h6" className={classes.title}>
+          Helpful Links
+        </Typography>
+        <Typography variant="body1">
+          <a href="#">Home</a> | <a href="#">About Us</a> |{' '}
+          <a href="#">Services</a> | <a href="#">Products</a> |{' '}
+          <a href="#">Careers</a> | <a href="#">Contact Us</a>
+        </Typography>
+      </Box>
+
+      {/* Third Section */}
+      <Box className={classes.section}>
+        <Typography variant="h6" className={classes.title}>
+          Contact Info
+        </Typography>
+        <Typography variant="body1">
+          Email: contact@example.com
+          <br />
+          Phone: (123) 456-7890
+        </Typography>
+      </Box>
+    </footer>
   );
 };
 
