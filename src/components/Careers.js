71d6f918277c0d75c7154aa04f2eee70eaab3e4b
ennics <ennics@gmail.com>
2023-10-03 15:37:12 -0400

    Began creating API for job postings. Testing fetch method with random api.

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -3,6 +3,13 @@ import React from 'react';
 import NavBar from './NavBar'
 import Footer from './Footer'
 import useScrollToTop from './useScrollToTop';
+import {Typography, Box, Button } from '@material-ui/core'; 
+
+const handleClick = () => {
+  fetch('https://catfact.ninja/fact')
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
 
 const Careers = () => {
   useScrollToTop();
@@ -11,6 +18,9 @@ const Careers = () => {
         <NavBar/>
         <h1>Careers</h1>
         <p>This is the Careers page content.</p>
+        <Button onClick={handleClick}>
+          GET
+        </Button>
         <Footer/>
     </div>
   );
