8cc0c487d72e948347235fe06d4b2660e99a722f
Nicholas Nicolaev <84345327+Ennics@users.noreply.github.com>
2023-10-17 17:52:31 -0400

    Completed AWS API for job postings.

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -6,10 +6,24 @@ import useScrollToTop from './useScrollToTop';
 import {Typography, Box, Button } from '@material-ui/core'; 
 
 const handleClick = () => {
-  fetch('https://catfact.ninja/fact')
+  fetch('https://ddz0uxvooi.execute-api.us-east-2.amazonaws.com/prod')
   .then(response => response.json())
   .then(data => console.log(data));
 }
+
+const targetDate = new Date('2023-09-01'); // Replace with your actual date
+
+// Current date
+const currentDate = new Date();
+
+// Calculate the time difference in milliseconds
+const timeDifference = currentDate - targetDate;
+
+// Calculate the number of days
+const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+// Display the result
+console.log(`Posted ${daysAgo} days ago`);
 
 // working on aws API
 
