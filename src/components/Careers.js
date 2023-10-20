4c1a48db5c53484b3a8914fca945bef3a44224cf
Nicholas Nicolaev <84345327+Ennics@users.noreply.github.com>
2023-10-20 16:51:02 -0400

    Added rendering for job postings from AWS API. Needs styling and query feature.

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,8 +1,9 @@
 // Careers.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from './NavBar'
 import Footer from './Footer'
 import useScrollToTop from './useScrollToTop';
+import { makeStyles } from '@material-ui/core/styles';
 import {Typography, Box, Button } from '@material-ui/core'; 
 
 const handleClick = () => {
@@ -11,32 +12,71 @@ const handleClick = () => {
   .then(data => console.log(data));
 }
 
-const targetDate = new Date('2023-09-01'); // Replace with your actual date
-
-// Current date
-const currentDate = new Date();
-
-// Calculate the time difference in milliseconds
-const timeDifference = currentDate - targetDate;
-
-// Calculate the number of days
-const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-// Display the result
-console.log(`Posted ${daysAgo} days ago`);
-
-// working on aws API
+const useStyles = makeStyles({
+  jobContainer: {
+    border: '1px solid #ccc',
+    padding: '16px',
+    margin: '16px',
+    borderRadius: '4px',
+  },
+  jobTitle: {
+    fontSize: '1.5rem',
+    fontWeight: 'bold',
+  },
+  location: {
+    fontSize: '1rem',
+  },
+  description: {
+    fontSize: '1rem',
+  },
+  requirements: {
+    fontSize: '1rem',
+  },
+});
 
 const Careers = () => {
   useScrollToTop();
+
+  const classes = useStyles();
+  const [jobs, setJobs] = useState([]);
+  
+  const apiUrl = 'https://ddz0uxvooi.execute-api.us-east-2.amazonaws.com/prod';
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // Convert the object of jobs into an array of job objects
+        const jobArray = Object.keys(data).map((jobName) => ({
+          jobName,
+          ...data[jobName],
+        }));
+        setJobs(jobArray);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div>
         <NavBar/>
         <h1>Careers</h1>
         <p>This is the Careers page content.</p>
-        <Button onClick={handleClick}>
-          GET
-        </Button>
+        <div>
+          <h1>Job Listings</h1>
+        </div>
+        {jobs.map((job, index) => (
+        <div key={index} className={classes.jobContainer}>
+          <h2 className={classes.jobTitle}>{job.jobName}</h2>
+          <p className={classes.datePosted}>Date Posted: {job['date-posted']}</p>
+          <p className={classes.description}>Description: {job.description}</p>
+          <p className={classes.type}>Type: {job.type}</p>
+          <p className={classes.requirements}>Requirements: {job.requirements.join(', ')}</p>
+          <p className={classes.location}>Location: {job.location}</p>
+          {job.salary && <p className={classes.salary}>Salary: {job.salary}</p>}
+        </div>
+      ))}
         <Footer/>
     </div>
   );
